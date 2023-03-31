6fc0340c52b78fe973248f1f4880658d9f6d8e7b
Seyi Oyemade <seyioyemade918@gmail.com>
2023-03-31 15:20:49 +0100

    Implement CRUD (create, read, update, delete) methods

---
+++ webpack/src/add-remove.js
@@ -0,0 +1,109 @@
+/*eslint-disable*/ 
+import {
+  count, ul, newListInput, getCountValue,
+} from './index.js';
+
+export const toggleList = () => {
+  const span = document.querySelectorAll('ul span');
+
+  span.forEach((item) => {
+    item.addEventListener('click', () => {
+      item.parentElement.parentElement.classList.add('bg-color');
+
+      const siblings = item.parentElement.parentElement.parentElement.children;
+
+      for (let i = 0; i < siblings.length; i += 1) {
+        if (siblings[i] !== item.parentElement.parentElement) {
+          if (siblings[i].classList.contains('bg-color')) {
+            siblings[i].classList.remove('bg-color');
+          }
+
+          if (siblings[i].lastElementChild.firstElementChild.classList.contains('fa-trash')) {
+            siblings[i].lastElementChild.firstElementChild.classList.remove('fa-trash');
+            siblings[i].lastElementChild.firstElementChild.classList.add('fa-ellipsis-v');
+          }
+        }
+
+        if (siblings[i] === item.parentElement.parentElement) {
+          if (siblings[i].lastElementChild.firstElementChild.classList.contains('fa-ellipsis-v')) {
+            siblings[i].lastElementChild.firstElementChild.classList.remove('fa-ellipsis-v');
+            siblings[i].lastElementChild.firstElementChild.classList.add('fa-trash');
+          }
+          if (siblings[i].lastElementChild.firstElementChild.classList.contains('fas')) {
+            siblings[i].lastElementChild.firstElementChild.classList.remove('fas');
+            siblings[i].lastElementChild.firstElementChild.classList.add('fa');
+          }
+        }
+      }
+    });
+  });
+};
+
+export const removeList = (arr) => {
+  const span = document.querySelectorAll('ul span');
+
+  span.forEach((item) => {
+    const trash = item.parentElement.nextElementSibling.lastElementChild;
+
+    trash.addEventListener('click', () => {
+      if (trash.classList.contains('fa-trash')) {
+        const siblings = item.parentElement.parentElement.parentElement.children;
+
+        for (let i = 0; i < siblings.length; i += 1) {
+          if (siblings[i] === item.parentElement.parentElement) {
+            const index = arr.findIndex((obj) => obj.id === Number(siblings[i].id));
+            arr.splice(index, 1);
+            item.parentElement.parentElement.classList.add('hide');
+            arr.forEach((obj) => {
+              if (obj.index > index) {
+                obj.index -= 1;
+              }
+            });
+
+            localStorage.setItem('storageToDoList', JSON.stringify(arr));
+            getCountValue();
+          }
+        }
+      }
+    });
+  });
+};
+
+export const editList = (arr) => {
+  const span = document.querySelectorAll('ul span');
+
+  span.forEach((item) => {
+    item.addEventListener('keyup', (event) => {
+      const siblings = item.parentElement.parentElement.parentElement.children;
+
+      for (let i = 0; i < siblings.length; i += 1) {
+        if (siblings[i] === item.parentElement.parentElement) {
+          const index = arr.findIndex((obj) => obj.id === Number(siblings[i].id));
+          arr[index].description = event.target.innerHTML;
+        }
+      }
+
+      localStorage.setItem('storageToDoList', JSON.stringify(arr));
+    });
+  });
+};
+
+export const addList = (arr) => {
+  const list = {
+    id: Math.floor(Math.random() * 10000000),
+    description: newListInput.value,
+    completed: [false],
+    index: count,
+  };
+
+  const li = `<li id = ${list.id}><div><input type="checkbox" name="check" class="checkbox">
+  <span contenteditable="true">${list.description}</span></div><div><i class="fas fa-ellipsis-v fa-1x"></i></div></li>`;
+  ul.innerHTML += li;
+  newListInput.value = '';
+  arr.push(list);
+  localStorage.setItem('storageToDoList', JSON.stringify(arr));
+
+  toggleList();
+  removeList(arr);
+  editList(arr);
+};
--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,30 +1,51 @@
 import './style.css';
+/*eslint-disable*/ 
+import {
+  addList, removeList, toggleList, editList,
+} from './add-remove.js';
 
-const ul = document.querySelector('ul');
+const toDoList = JSON.parse(localStorage.getItem('storageToDoList')) || [];
+const enterKey = document.querySelector('.enter-key');
+export const newListInput = document.querySelector('.add-input');
+export const ul = document.querySelector('ul');
+/*eslint-disable*/ 
+export let count;
 
-const toDoList = [
-  {
-    description: 'wash the dishes',
-    completed: [true, false],
-    index: 0,
-  },
-  {
-    description: 'sweep the floor',
-    completed: [true, false],
-    index: 1,
-  },
-  {
-    description: 'visit the mall',
-    completed: [true, false],
-    index: 2,
-  },
-];
-
-const loopArray = () => {
-  for (let i = 0; i < toDoList.length; i += 1) {
-    const li = `<li><div><input type="checkbox" name="check" class="checkbox"><span>${toDoList[i].description}</span></div> <i class="fas fa-ellipsis-v fa-1x"></i></li>`;
+const getStorageItem = () => {
+  toDoList.forEach((list) => {
+    const li = `<li id = ${list.id}><div><input type="checkbox" name="check" class="checkbox"><span contenteditable="true">${list.description}</span></div><div><i class="fas fa-ellipsis-v fa-1x"></i></div></li>`;
     ul.innerHTML += li;
+  });
+};
+
+export const getCountValue = () => {
+  if (toDoList.length === 0) {
+    count = 1;
+  } else if (toDoList[toDoList.length - 1].index >= 1) {
+    count = toDoList[toDoList.length - 1].index + 1;
+  } else {
+    count = 1;
   }
 };
 
-loopArray();
+enterKey.addEventListener('click', () => {
+  if (newListInput.value.length > 0) {
+    addList(toDoList);
+    count += 1;
+  }
+});
+
+newListInput.addEventListener('keydown', (event) => {
+  if (newListInput.value.length > 0) {
+    if (event.key === 'Enter') {
+      addList(toDoList);
+      count += 1;
+    }
+  }
+});
+
+getCountValue();
+getStorageItem();
+editList(toDoList);
+removeList(toDoList);
+toggleList();
