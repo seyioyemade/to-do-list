ffe1d8ce08cd01a61751e393bbcc528840ef881d
Seyi Oyemade <seyioyemade918@gmail.com>
2023-04-07 15:10:02 +0100

    Add describe method to test file

--- webpack/src/tests/edit-update-completed.test.js
+++ webpack/src/tests/edit-update-completed.test.js
@@ -2,87 +2,91 @@ jest.mock('../index.js');
 
 import { testFunctions } from '../index.js';
 
-test('Editing a task', () => {
+describe('Methods for editing, updating and verifying completed task(s)', () => {
 
-  const list = {
-    index: 2,
-    description: 'reading books',
-    completed: false,
-  }
+  test('Editing a task', () => {
 
-  const newDescription = 'sweeping the floor';
-
-  const result = [
-    {
-    index: 1,
-    description: 'going to the restaurant',
-    completed: false,
-    },
-    {
+    const list = {
       index: 2,
-      description: 'sweeping the floor',
+      description: 'reading books',
       completed: false,
-    },
-  ]
-  
-  const editItem = new testFunctions();
-  
-  expect(editItem.editTask(list, newDescription)).toStrictEqual(result);
-});
-
-test('Updating an item completed status', () => {
-
-  const list = {
-    index: 1,
-    description: 'going to the restaurant',
-    completed: false,
-  }
-
-  const result = [
-    {
-      index: 1,
-      description: 'going to the restaurant',
-      completed: true,
-      },
-  
-      {
-        index: 2,
-        description: 'reading books',
-        completed: false,
-      },
-  ];
-
-  const updateStatus = new testFunctions();
-  
-  expect(updateStatus.updateCompletedStatus(list)).toStrictEqual(result);
-
-});
-
-
-test('Clear all completed tasks', () => {
-
-  const list = {
-    index: 3,
-    description: 'washing clothes',
-    completed: true,
     }
 
-  const result = [
-    {
+    const newDescription = 'sweeping the floor';
+
+    const result = [
+      {
       index: 1,
       description: 'going to the restaurant',
       completed: false,
       },
-  
       {
         index: 2,
-        description: 'reading books',
+        description: 'sweeping the floor',
         completed: false,
       },
-  ]
+    ]
+    
+    const editItem = new testFunctions();
+    
+    expect(editItem.editTask(list, newDescription)).toStrictEqual(result);
+  });
 
-  const clearAll = new testFunctions();
-  
-  expect(clearAll.clearAllCompletedStatus(list)).toStrictEqual(result);
+  test('Updating an item completed status', () => {
+
+    const list = {
+      index: 1,
+      description: 'going to the restaurant',
+      completed: false,
+    }
+
+    const result = [
+      {
+        index: 1,
+        description: 'going to the restaurant',
+        completed: true,
+        },
+    
+        {
+          index: 2,
+          description: 'reading books',
+          completed: false,
+        },
+    ];
+
+    const updateStatus = new testFunctions();
+    
+    expect(updateStatus.updateCompletedStatus(list)).toStrictEqual(result);
+
+  });
+
+
+  test('Clear all completed tasks', () => {
+
+    const list = {
+      index: 3,
+      description: 'washing clothes',
+      completed: true,
+      }
+
+    const result = [
+      {
+        index: 1,
+        description: 'going to the restaurant',
+        completed: false,
+        },
+    
+        {
+          index: 2,
+          description: 'reading books',
+          completed: false,
+        },
+    ]
+
+    const clearAll = new testFunctions();
+    
+    expect(clearAll.clearAllCompletedStatus(list)).toStrictEqual(result);
+
+  });
 
 });
